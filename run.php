3a13d467b8e55af27a28fc6024ad1ecaa987c479
Christoph Breit <christoph.breit@netways.de>
2026-04-09 13:55:42 +0200

    refactor return for hook to make icingaweb render logic work correctly

--- run.php
+++ run.php
@@ -1,14 +1,11 @@
 <?php
 
-use Icinga\Module\Perfdatagraphs\ProvidedHook\Icingadb\IcingadbSupport;
-
 /** @var $this \Icinga\Application\Modules\Module */
 
+$section = $this->menuSection(N_('Graphite'), ['icon' => 'chart-area']);
 $this->provideHook('icingadb/IcingadbSupport');
 $this->provideHook('icingadb/ServiceDetailExtension');
 $this->provideHook('icingadb/HostDetailExtension');
 
-// This is needed because IcingaDB also calls this hooks and we want to avoid this
-if (! static::exists('icingadb') || ! IcingadbSupport::useIcingaDbAsBackend()) {
-    $this->provideHook('monitoring/DetailviewExtension');
-}
+$this->provideHook('monitoring/DetailviewExtension');
+$this->provideHook('monitoring/DetailviewExtension');
